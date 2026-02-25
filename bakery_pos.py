102e07a4310e7b97cb1097470f84b3d9f11060b8
syedsajjad325-art <syedsajjad325@gmail.com>
2026-02-25 18:49:22 +0000

    Create bakery_pos.py
    Added Bakery POS system with discount logic.

---
+++ bakery_pos.py
@@ -0,0 +1,20 @@
+Grand_Total = 0
+print("Welcome to the Bakery POS System")
+
+name = input("Cashier Name: ")
+count = int(input("Number of Items: "))
+
+# 1. Collect all prices first
+for i in range(count):
+    price = float(input(f"Enter the price for item {i+1}: "))
+    Grand_Total = Grand_Total + price
+
+# 2. Apply discount on the TOTAL (If more than 20 KD)
+if Grand_Total >= 20:
+    print("--- You earned a 10% Ramadan Discount! ---")
+    Grand_Total = Grand_Total * 0.90 
+
+# 3. Final Output
+print("-" * 30)
+print(f"Final Total Bill for {name}'s station: {Grand_Total:.3f} KD")
+print("Thank you for shopping with us!")
